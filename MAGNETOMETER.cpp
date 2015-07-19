c3bfa786ce9186df55899f12c586dd41a50ba2a3
louisfaury <l.faury@hotmail.fr>
2015-07-19 13:25:36 -0500

    Adding getHeading() method

--- MAGNETOMETER.cpp
+++ MAGNETOMETER.cpp
@@ -9,6 +9,9 @@
 #include "MAGNETOMETER.h"
 
 
+#define TODEG 57.295791
+
+
 
 MAGNETOMETER::MAGNETOMETER(){
 }
@@ -24,3 +27,71 @@ MAGNETOMETER::MAGNETOMETER(const std::string mag_file_name, STYLE style) : Capto
         // TODO
     }
 }
+
+
+
+int MAGNETOMETER::getHeading(Eigen::Vector3f PRY) const{
+    // heading_x = magx*cos(pitch)-magy*sin(roll)cos(pitch)-magz*cos(roll)sin(pitch)
+    int heading_x = mag_x*cos(PRY(1))-mag_y*sin(PRY(0))*sin(PRY(1))-mag_z*cos(PRY(0))*sin(PRY(1));
+    // heading_y = magy*cos(roll)-magz*sin(roll)
+    int heading_y = mag_y*cos(PRY(0))-mag_z*sin(PRY(0));
+    
+    return (int)(TODEG*atan2(heading_y, heading_x));
+}
+
+
+
+void MAGNETOMETER::update(){
+    int count = 0;
+    std::string::size_type sz;
+    if (std::getline(captor_file,line)){
+        line_end = false;
+        std::stringstream ss(line);
+        while (getline(ss,line,',')){
+            if (line == LINE_MARK);
+            else {
+                switch (count) {
+                    case 0:
+                        mag_x = std::stof(line,&sz);
+                        break;
+                    case 1:
+                        mag_y = std::stof(line,&sz);
+                        break;
+                    case 2:
+                        mag_z = std::stof(line,&sz);
+                        break;
+                }
+                count++;
+            }
+        }
+        std::cout << captor_id << " : line read" << std::endl;
+    }
+    else{
+        std::cout << "Couldn't read from captor : " << captor_id << std::endl;
+        line_end = true;
+    }
+
+}
+
+
+void MAGNETOMETER::correct(){
+    mag_x += (int)captor_offsets(0);
+    mag_y += (int)captor_offsets(1);
+    mag_z += (int)captor_offsets(2);
+}
+
+
+
+void MAGNETOMETER::update_correct(){
+    this->update();
+    this->correct();
+}
+
+
+void MAGNETOMETER::printState(){
+    std::cout << mag_x << "\t" << mag_y << "\t" << mag_z << std::endl;
+}
+
+
+
+
--- MAGNETOMETER.h
+++ MAGNETOMETER.h
@@ -20,6 +20,45 @@ public:
     
     MAGNETOMETER(const std::string, STYLE);
     
+    
+    
+    
+    
+    
+   /* \brief Returns the inertial unit heading
+    * \brief Deprecated, we will eventually be using DCM Matrix method. 
+    * \param A 3 columns vector (actually we will be using only roll & pitch but to make it easier we take the whole PRY vector as argument)
+    */
+    int getHeading(Eigen::Vector3f PRY) const;
+    
+    
+    
+    
+    
+   /* TODO
+    */
+   // int getHeading(Eigen::Matrix3f DCM);
+    
+    
+    
+    void update();
+    
+    
+    
+    void correct();
+    
+    
+    
+    void update_correct();
+    
+    
+    void printState();
+    
+    
+private:
+    
+    int mag_x, mag_y, mag_z;
+
 };
 
 #endif /* defined(__INS_EKF__MAGNETOMETER__) */
