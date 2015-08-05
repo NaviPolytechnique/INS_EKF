a8ff216fa2505ee7a50a7ec6eb6f79af6b122676
louisfaury <l.faury@hotmail.fr>
2015-08-05 08:13:31 -0500

    Testing new methods

--- main.cpp
+++ main.cpp
@@ -57,12 +57,13 @@ int main(int argc, const char * argv[]) {
         MAGNETOMETER mag(mag_file_path, Captor::COLD_START);
         GPS gps(gps_file_path,GPS::NMEA);
         GPS_Filter gps_filter(&gps);
-        EKF ekf(&gps_filter,&acc,&gyro);
+        EKF ekf(&gps_filter,&acc,&gyro,&mag);
         
     
     
     
     // Getting offset and gain for accelerometer, gyroscope and magnetometer. Getting GPS HOME field.
+    // This routine will warn you if one of the offset couldn't be updated
         int ret = acc.initOffsets();
         if (ret != 1) std::cout << "Couldn't get acc offsets !" << std::endl;
         else std::cout << "Acc offsets updated" << std::endl;
@@ -87,54 +88,63 @@ int main(int argc, const char * argv[]) {
         
         
         
-    // Reading line
+    // Reading lines
+        // We create the buffer for the sensor's output values
         Eigen::Vector3f acc_vector_buffer;
         Eigen::Vector3f gyro_vector_buffer;
         Eigen::Vector3d gps_buffer_vector;
         Eigen::Vector3d gps_position_vector;
         
-       
+       // We update and correct the magnetometer
+        mag._init_earth_even_magnetic_field();
         mag.update_correct();
+        // We initialize the state vector giving the initial heading
         ekf.init_state_vector(mag.getHeading());
-        std::cout << ekf.get_state_vector().transpose() << std::endl;
         //ekf.init_state_vector();
-        to_rpy << TODEG*(ekf.toRPY(ekf.get_state_vector())).transpose() << std::endl;
-
+        to_rpy << TODEG*(ekf.toRPY(ekf.get_state_vector())).transpose() << std::endl;           // Storing operation
 
         
         
         while (!acc.line_end && !gyro.line_end){
             
-            //Reading from inertial captors
+            // Reading from inertial captors and correcting the outputs
             acc.getOutput(&acc_vector_buffer);
             acc.correctOutput(&acc_vector_buffer);
             gyro.getOutput(&gyro_vector_buffer);
             gyro.correctOutput(&gyro_vector_buffer);
+            // Updating and correcting magnetometer
             mag.update_correct();
+            // Updating GPS and storing into the &gps_buffer_vector
             gps.update(&gps_buffer_vector);
             
             
         
-            //Predicting state vector
+            // Predicting the state vector
+            // Extended Kalman Filter according step is the prediction step. We first build the Jacobian matrix linearized around the current state and we then multiply the currrent state vector by such a matrix
             ekf.build_jacobian_matrix(&acc_vector_buffer, &gyro_vector_buffer);
             ekf.predict();
-            to_rpy << TODEG*(ekf.getRPY()).transpose() << std::endl;
-            to_log << ekf.get_state_vector().transpose() << std::endl;
-            //mag_out << TODEG*mag.getHeading() << std::endl;
-            mag_out << TODEG*mag.getHeading((ekf.getRPY())(1),(ekf.getRPY())(0)) << std::endl;
+            
+            to_rpy << TODEG*(ekf.getRPY()).transpose() << std::endl;                            // Storing operation for data exploitation
+            to_log << ekf.get_state_vector().transpose() << std::endl;                          // Storing operation for data exploitation
+            //mag_out << TODEG*mag.getHeading() << std::endl;                                   // Storing operation for data exploitation
+            mag_out << TODEG*mag.getHeading((ekf.getRPY())(1),(ekf.getRPY())(0)) << std::endl;  // Storing operation for data exploitation
 
             
     
 
             
-            // Testing GPS to Cartesian and magnetometer tilt correction
+            // Checking inf a new gps data is availaible
             if (gps.isAvailable()){
+                // If yes, we first update the GPS filter
                 gps.calculatePositionFromHome(&gps_buffer_vector);
                 gps_out << gps.getPositionFromHome(&gps_buffer_vector).transpose() << std::endl;
                 gps.actualizeInternDatas(&gps_buffer_vector);
                 gps_filter.predict();
                 gps_filter.updateFilter();
-                gps_filter_out << gps_filter.getState().transpose() << std::endl;
+                gps_filter_out << gps_filter.getState().transpose() << std::endl;               // Storing operation for data exploitation
+                
+                // And we then correct the EKF filter by correcting in order position, speed and quaternion attitude vector
+                ekf.correct();
             }
             
         }
